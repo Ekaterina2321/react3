0e19943ead48657db5444ed5465eae3f83dd43d5
Ekaterina2321 <ekaterina_sandakova2@mail.ru>
2024-10-08 14:46:11 +0300

    Практика 3 react

---
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -0,0 +1,10 @@
+import { Catalog } from "../../components/Catalog/Catalog";
+
+
+export function CatalogPage() {
+    return ( 
+        <section>
+            <Catalog/>
+        </section>
+    )
+}
